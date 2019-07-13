97fc48a01905dd131dac5ab799a127191e61d6f4
quoctuanuit <quoctuanuit@gmai.com>
2019-07-13 21:53:20 +0700

    First commit

---
+++ PricesCollector/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace PricesCollector
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnParse = new System.Windows.Forms.Button();
+            this.txtUrl = new System.Windows.Forms.RichTextBox();
+            this.txtOutput = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // btnParse
+            // 
+            this.btnParse.Location = new System.Drawing.Point(1048, 12);
+            this.btnParse.Name = "btnParse";
+            this.btnParse.Size = new System.Drawing.Size(120, 59);
+            this.btnParse.TabIndex = 0;
+            this.btnParse.Text = "Search";
+            this.btnParse.UseVisualStyleBackColor = true;
+            this.btnParse.Click += new System.EventHandler(this.btnParse_Click);
+            // 
+            // txtUrl
+            // 
+            this.txtUrl.Location = new System.Drawing.Point(12, 12);
+            this.txtUrl.Name = "txtUrl";
+            this.txtUrl.Size = new System.Drawing.Size(1014, 59);
+            this.txtUrl.TabIndex = 1;
+            this.txtUrl.Text = "https://tiki.vn/totolink-n150usm-usb-wifi-chuan-n-toc-do-150mbps-p481696.html?spi" +
+    "d=9799612";
+            this.txtUrl.TextChanged += new System.EventHandler(this.txtUrl_TextChanged);
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Location = new System.Drawing.Point(12, 154);
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.Size = new System.Drawing.Size(1156, 493);
+            this.txtOutput.TabIndex = 2;
+            this.txtOutput.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1180, 659);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.txtUrl);
+            this.Controls.Add(this.btnParse);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnParse;
+        private System.Windows.Forms.RichTextBox txtUrl;
+        private System.Windows.Forms.RichTextBox txtOutput;
+    }
+}
+
---
+++ PricesCollector/Form1.cs
@@ -0,0 +1,114 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PricesCollector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public string ReplaceAt(string input, int index, char newChar)
+        {
+            if (input == null)
+            {
+                throw new ArgumentNullException("input");
+            }
+            StringBuilder builder = new StringBuilder(input);
+            builder[index] = newChar;
+            return builder.ToString();
+        }
+
+        private void btnParse_Click(object sender, EventArgs e)
+        {
+            string expireStr = "2019-07-30";
+            DateTime expireDate = Convert.ToDateTime(expireStr);
+
+            if(DateTime.Now > expireDate)
+            {
+                MessageBox.Show("Trial version expired!");
+                this.Close();
+                return;
+            }
+
+            if(!txtUrl.Text.Contains("tiki.vn"))
+            {
+                MessageBox.Show("Only support tiki.vn!");
+                txtOutput.Text = "";
+                return;
+            }
+
+            string selectedLine = "";
+            string price = "";
+
+            using (var client = new WebClient())
+            using (var stream = client.OpenRead(txtUrl.Text))
+            using (var reader = new StreamReader(stream))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.Contains("otherSeller"))
+                    {
+                        selectedLine = line;
+                        break;
+                    }
+                }
+            }
+
+            string jsonStr = selectedLine.Trim();
+
+            //head
+            jsonStr = jsonStr.Replace("var ", "{");
+            int pos = jsonStr.IndexOf("=");
+            jsonStr = ReplaceAt(jsonStr, pos, ':');
+
+            //tail
+            pos = jsonStr.LastIndexOf(";");
+            if (pos >= 0)
+            {
+                jsonStr = jsonStr.Remove(pos);
+            }
+            jsonStr += "}";
+
+            try
+            {
+                JObject jsonObject = JObject.Parse(jsonStr);
+
+                Console.WriteLine(jsonObject);
+
+                foreach (var obj in jsonObject["otherSeller"])
+                {
+                    Console.WriteLine( obj["link"] );
+                    price += obj["name"] + ":\n";
+                    price += obj["price"] + " VND\n";
+                    price += "\n";
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            txtOutput.Text = price;
+        }
+
+        private void txtUrl_TextChanged(object sender, EventArgs e)
+        {
+            txtOutput.Text = "";
+        }
+    }
+}
